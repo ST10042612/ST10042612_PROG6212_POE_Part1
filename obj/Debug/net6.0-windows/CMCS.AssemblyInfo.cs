dfbade68568a9e90ee2991c313f02e11507e0077
OPeverything <83116734+OPeverything@users.noreply.github.com>
2024-09-10 15:58:26 +0200

    Addition of all Windows

--- obj/Debug/net6.0-windows/CMCS.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/CMCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9ce303b183e1df10f7d8f49d86138f94fc2142")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
